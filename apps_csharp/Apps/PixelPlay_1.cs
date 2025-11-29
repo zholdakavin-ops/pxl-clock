e9dd3dc88f961d886b5826cfa8039de7896c88de
SchlenkR <SchlenkR@github>
2025-11-29 18:35:35 +0100

    x

--- apps_csharp/Apps/PixelPlay_1.cs
+++ apps_csharp/Apps/PixelPlay_1.cs
@@ -5,27 +5,29 @@ static class PixelPlay_1
 {
     public static Action Scene = () =>
     {
+        // Blue background
         Ctx.Background().Solid(Color.Blue);
 
-        var pixels = Ctx.Pixels();
-
+        // Random colored pixels
         var random = new Random();
-        for (int i = 0; i < pixels.Length; i++)
+        for (int i = 0; i < Ctx.Pixels().Length; i++)
         {
-            pixels[i] = Color.FromRgb(
+            Ctx.Pixels()[i] = Color.FromRgb(
                 (byte)random.Next(256),
                 (byte)random.Next(256),
                 (byte)random.Next(256));
         }
 
+        // Black "HELLO" text at (0,10)
         Ctx.Text().Mono4x5("HELLO", 0, 10, Color.Black);
 
-        for (int i = 0; i < pixels.Length; i++)
+        // Change black pixels to blue (actually, the text pixels)
+        for (int i = 0; i < Ctx.Pixels().Length; i++)
         {
-            var color = pixels[i];
+            var color = Ctx.Pixels()[i];
             if (color.Red == 0 && color.Green == 0 && color.Blue == 0)
             {
-                pixels[i] = Color.Blue;
+                Ctx.Pixels()[i] = Color.Blue;
             }
         }
     };
